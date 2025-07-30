4cf8e7d179fce93575f49fff3722ba5085cd95fd
Harsh <userharsh911@gmail.com>
2025-07-30 14:07:30 +0530

    first commit

--- app.js
+++ app.js
@@ -6,19 +6,21 @@ const {hostRouter} = require("./routes/hostRouter")
 const {pageNotFoundController} = require("./controllers/error");
 const { default: mongoose } = require("mongoose");
 const authRouter = require("./routes/authRouter");
+// import dotenv from "dotenv"
+const dotenv = require("dotenv")
 const session = require("express-session")
 const mongoStore = require("connect-mongodb-session")(session)
-const DB_PATH = "mongodb+srv://userharsh911:aqMvAtWcl62PC3M7@cluster0.ddrr2.mongodb.net/airbnb?retryWrites=true&w=majority&appName=Cluster0";
+// const DB_PATH = "mongodb+srv://userharsh911:aqMvAtWcl62PC3M7@cluster0.ddrr2.mongodb.net/airbnb?retryWrites=true&w=majority&appName=Cluster0";
 
 const app = express();
-
+console.log("process ",process.env.DB_PATH)
 app.use(express.urlencoded())
 app.set('view engine','ejs')
 app.set('views','views')
 app.use(express.static(path.join(rootdir,"public")))
 
 const store = mongoStore({
-    uri : DB_PATH,
+    uri : process.env.DB_PATH,
     collection: 'sessions'
 })
 app.use(session({
@@ -49,13 +51,10 @@ app.use("/host",hostRouter);
 app.use(userRouter);
 app.use(pageNotFoundController)
 
-const PORT = 3001;
 
-mongoose.connect(DB_PATH)
+mongoose.connect(process.env.DB_PATH)
 .then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`the server is running on http://localhost:${PORT}`);
-    })
+    console.log("connected ")
 })
 .catch((err)=>{
     console.log("error while connecting to mongodb : ",err)
